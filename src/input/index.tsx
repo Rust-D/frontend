e8a79845ff7491bca4221ca48ff724ff3c39c37c
sungyo <hchengjian1@gmail.com>
2022-12-03 18:52:26 +0900

    [Update] 関数名の変更

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from'react-dom/client';
 import App from "./App"
 
-function Test(){
+function Input(){
     return(
         <div>
             <App />
@@ -11,4 +11,4 @@ function Test(){
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<Test />);
+root.render(<Input />);
