d0997fc23a6b7334f01ed96dc1249a95a704b0ec
sungyo4869 <hchengjian1@gmail.com>
2022-12-04 03:43:41 +0900

    [Add] 各自のフォルダ、node_modulesの追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
+import React from 'react'
+import ReactDOM from'react-dom/client';
+import App from "./App"
 
-export default function Main() {
-    return (
+function Test(){
+    return(
         <div>
-            <p>unko</p>
+            <App />
         </div>
-    )
+    );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
-root.render(<Main />)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<Test />);
