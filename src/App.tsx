ba27f00c0d50dcf71848a4d932b6d4392deca68c
yuha <issiy0126@gmail.com>
2022-12-04 23:09:56 +0900

    update: import conporInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import PuresentInput from './input/App';
 
 export default function App(){
     return(
         <div className = 'App'>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path='/' /*component={homepage}*/ />
-                    <Route path='/presents/input' /*component={presentsInput}*/ />
+                    <Route exact path='/' /*component={}*/ />
+                    <Route path='/presents/input' component={PuresentInput} />
                     <Route path='/presents/result' /*component={presentsResult}*/ />
                     <Route path='/questionnaire/input' /*component={questionnaireInput}*/ />
                     <Route path='/questionnaire/result' /*component={questionnaireResult}*/ />
