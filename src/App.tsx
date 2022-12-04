d725ba9d7e42c3046e5656bd5f0147ae9436ffc1
yuha <issiy0126@gmail.com>
2022-12-04 15:38:40 +0900

    update: Routing実装(仮)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 export default function App(){
     return(
         <div className = 'App'>
-            Hello, World!
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' /*component={homepage}*/ />
+                    <Route path='/presents/input' /*component={presentsInput}*/ />
+                    <Route path='/presents/result' /*component={presentsResult}*/ />
+                    <Route path='/questionnaire/input' /*component={questionnaireInput}*/ />
+                    <Route path='/questionnaire/result' /*component={questionnaireResult}*/ />
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
