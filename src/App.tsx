287934a965ff3745ebc3f981844b473871d5aecb
chusehi <yu.nakamura.gamegod@gmail.com>
2022-12-07 02:19:45 +0900

    HomeViewをルート追加した

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import presentInput from './input/App';
 import questionnaireInput from './input_diagnosis/App'
+import HomeView from './home/HomeView';
 
 export default function App(){
     return(
         <div className = 'App'>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path='/' /*component={}*/ />
+                    <Route exact path='/' component={HomeView} />
                     <Route path='/presents/input' component={presentInput} />
                     <Route path='/presents/result' /*component={presentsResult}*/ />
                     <Route path='/questionnaire/input' component={questionnaireInput}/>
