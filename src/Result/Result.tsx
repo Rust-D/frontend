2ecad5d6cf9f7493380a0f34a845811c2cd15e1f
yuha <issiy0126@gmail.com>
2022-12-07 22:40:24 +0900

    update: itemコンテナを作った

--- src/Result/Result.tsx
+++ src/Result/Result.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { GiftRepository } from '../data/GiftRepository'
 
 function Result() {
+
+    const GiftRepo = new GiftRepository
+    const [Gifts, SetGifts] = useState(GiftRepo.getRecommendedGiftList(/*nanikakaku*/))
+
+    
+  
   return (
     <div>提案結果</div>
+    
+
     
   )
 }
