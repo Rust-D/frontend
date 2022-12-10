8dd5061c5e9733de9ea50aae5ca368c311755002
yuha <issiy0126@gmail.com>
2022-12-10 20:19:20 +0900

    update Loading画面適応準備

--- src/LoadingPage/LoadingPage.tsx
+++ src/LoadingPage/LoadingPage.tsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 import './LoadingPage.css'
 
-export default function LoadingPage (){
-  return (
-    <div id='overlay'>
-      <div className="loading">
-        <span className="circle"></span>
-      </div>
-        <div className='text'>
-            提案中
+type ShowProps = {
+    isShow: boolean
+};
+
+export default function LoadingPage (LoadingState : ShowProps){
+  if(LoadingState.isShow) {
+    return (
+      <div id='overlay'>
+        <div className="loading">
+          <span className="circle"></span>
         </div>
-    </div>
-  )
+          <div className='text'>
+              提案中
+          </div>
+      </div>
+    )
+  }else{
+    return null;
+  }
 }
