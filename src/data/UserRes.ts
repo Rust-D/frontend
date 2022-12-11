8e66aeffb0168ac842dd8d0ab8de8d1785ae1b4c
yuha <issiy0126@gmail.com>
2022-12-12 00:50:45 +0900

    update: UserResのクラスを作成

---
+++ src/data/UserRes.ts
@@ -0,0 +1,11 @@
+import { Condition } from "./Conditions";
+
+export class UserRes  {
+    UCondition : Condition;
+    URes : string;
+
+    constructor(UCondition: Condition, URes: string) {
+        this.UCondition = UCondition;
+        this.URes = URes;
+    }
+};
