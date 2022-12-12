58cc59e2cda194db1ee6054f9ee811738046dcf1
yuha <issiy0126@gmail.com>
2022-12-13 00:28:13 +0900

    再提案とUserRes用のラジオボタンを追加

--- src/Result/QuestionaireResult/QuestionaireItem.tsx
+++ src/Result/QuestionaireResult/QuestionaireItem.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { isTemplateMiddleOrTemplateTail } from 'typescript'
 import { Gift } from '../../data/Gift'
 type ItemProp = {
     gift: Gift
     OnRadio: Function
 }
+
+const ClickRadio = () => {
+  
+} 
 const Item = (prop: ItemProp) => {
+  const ClickRadio = () => {
+    prop.OnRadio(prop.gift)
+  } 
   return (
     <div>
-      <input type="radio" name="user_res" value={prop.gift.name} onClick = {prop.OnRadio} />{Gift.name}
+      <input type="radio" name="user_res" value={prop.gift.name} onClick = {ClickRadio} />{Gift.name}
     </div>
   )
 }
