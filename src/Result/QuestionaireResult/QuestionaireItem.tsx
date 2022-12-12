deca4642d5cfab713586095851e2d2636781f97e
yuha <issiy0126@gmail.com>
2022-12-12 23:38:17 +0900

    update: add radio button

--- src/Result/QuestionaireResult/QuestionaireItem.tsx
+++ src/Result/QuestionaireResult/QuestionaireItem.tsx
@@ -6,7 +6,9 @@ type ItemProp = {
 }
 const Item = (prop: ItemProp) => {
   return (
-    <div>Item</div>
+    <div>
+      <input type="radio" name="user_res" value={prop.gift.name} onClick = {prop.OnRadio} />{Gift.name}
+    </div>
   )
 }
 
