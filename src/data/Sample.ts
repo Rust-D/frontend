4ef590e43995f632e5872f9431ed36db728a4454
yuha <issiy0126@gmail.com>
2022-12-13 17:09:47 +0900

    update Resultの見た目だけ

---
+++ src/data/Sample.ts
@@ -0,0 +1,15 @@
+import { Condition } from "./Conditions";
+
+export const condition:Condition = {
+    maxPrice: 200,
+    minPrice: 1,
+    profile: {
+        age: "around_10",
+        sex: "female",
+    },
+    season: "spring",
+    giftCategory: [
+        "fashion"
+    ]
+
+}
