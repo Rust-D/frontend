223b78a4483bace2eb0ef79a0c5a6fd10b4f50de
yuha <issiy0126@gmail.com>
2022-12-14 15:28:41 +0900

    update Test -> getRecommendedGiftList

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import presentInput from './input/input';
 import questionnaireInput from './input/inputDiagnose';
 import QResult from './Result/QuestionaireResult/questionaireResult';
-
+import PResult from './Result/PresentResult/Result';
 import HomeView from './home/HomeView';
 
 export default function App(){
@@ -13,7 +13,7 @@ export default function App(){
                 <Switch>
                     <Route exact path='/' component={HomeView} />
                     <Route path='/presents/input' component={presentInput} />
-                    <Route path='/presents/result' /*component={presentsResult}*/ />
+                    <Route path='/presents/result' component={PResult} />
                     <Route path='/questionnaire/input' component={questionnaireInput}/>
                     <Route path='/questionnaire/result' component={QResult} />
                 </Switch>
